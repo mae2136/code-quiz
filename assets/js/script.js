39f894dccd2ff939119d433b1fa1b8da6e576516
Miguel Escobar <mae2136@gmail.com>
2022-02-21 22:27:48 -0600

    index.html: - Update button selectors in answer list to classes.
    script.js:
    - Finish function to display questions.
    - Start function to check answer. Ensure it works.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,10 @@ var mainHeaderDiv = document.querySelector("#mainHeader");
 var startButton = document.querySelector("#startButton");
 var information = document.querySelector("#information");
 var possibleAnswers = document.querySelector("#answer");
-var answerList = document.querySelector("#answerList");
+var answerListDisplay = document.querySelector("#answerList");
+var answerList = document.querySelectorAll("button.answer");
 console.log(mainHeaderDiv);
-console.log(answerList.children);
+console.log(answerList);
 
 // Array of Question Headers
 var questionHeader = ["Question 1", "Question 2", "Question 3", "Question 4"];
@@ -18,6 +19,8 @@ var answers = {
     question4: [`a`, `b`, `c`, `d`],
     question5: [`a`, `b`, `c`, `d`]
 }
+// Tracks how many questions have been asked.
+var questionCounter;
 
 console.log(answers);
 
@@ -26,14 +29,13 @@ startButton.addEventListener("click", startQuiz);
 
 // Starts quiz on start button click.
 function startQuiz(event) {
-    console.log("hello world");
-    // Tracks how many questions have been asked.
-    var questionCounter = 0;
+    questionCounter = 0;    
     // Remove start button
     startButton.style.display = "none";
     // Replace initial page text with first question
-    showQuestion(questionCounter);
-    // Start timer (interval)
+    answerListDisplay.style.display = "block";
+    questionCounter = showQuestion(questionCounter);
+    // Start timer (interval) for 60 seconds
 }
 
 // Shows a quiz question based on which question was previously answered
@@ -43,21 +45,63 @@ function showQuestion(questionCounter) {
             mainHeaderDiv.textContent = questionHeader[questionCounter];
             information.textContent = question[questionCounter];
             questionCounter++;
+            for (let i = 0; i < answerList.length; i++) {
+                answerList[i].textContent = answers.question1[i];
+                console.log(answerList[i]);
+            }
             break;
         case 1:
-            mainHeaderDiv.textContent = "Question 1";
-            information.textContent = "Which of these is not an option for the display CSS feature?";
+            mainHeaderDiv.textContent = questionHeader[questionCounter];
+            information.textContent = question[questionCounter];
+            for (let i = 0; i < answerList.length; i++) {
+                answerList[i].textContent = answers.question2[i];
+                console.log(answerList[i]);
+            }
+            questionCounter++;
+            break;
+        case 2:
+            mainHeaderDiv.textContent = questionHeader[questionCounter];
+            information.textContent = question[questionCounter];
+            for (let i = 0; i < answerList.length; i++) {
+                answerList[i].textContent = answers.question3[i];
+                console.log(answerList[i]);
+            }
+            questionCounter++;
+            break;
+        case 3:
+            mainHeaderDiv.textContent = questionHeader[questionCounter];
+            information.textContent = question[questionCounter];
+            for (let i = 0; i < answerList.length; i++) {
+                answerList[i].textContent = answers.question4[i];
+                console.log(answerList[i]);
+            }
+            questionCounter++;
+            break;
+        case 4:
+            mainHeaderDiv.textContent = questionHeader[questionCounter];
+            information.textContent = question[questionCounter];
+            for (let i = 0; i < answerList.length; i++) {
+                answerList[i].textContent = answers.question5[i];
+                console.log(answerList[i]);
+            }
             questionCounter++;
             break;
         default:
             break;
     }
+    return questionCounter;
 }
 // Check answer for right or wrong (click eventlistener)
+answerListDisplay.addEventListener("click", checkAnswer);
+
 // Reduce timer by 10 seconds if answer is incorrect
-
 // Proceed to the next each question regardless of answer
+function checkAnswer(event) {
+    console.log(event.target);
+    console.log(questionCounter);
+}
 
+// If timer = 0, ask user to input initials with score of 0
 // After final question, ask user to input initials
 // Open new page to display score
 // Store score and name in local storage
