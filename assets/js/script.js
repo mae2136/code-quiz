8f0f92801977d9793e693f307f3dd6fddc216dde
Miguel Escobar <mae2136@gmail.com>
2022-02-21 19:43:11 -0600

    index.html: - Updated button list selectors and style.
    script.js:
    -Add initial comments/pseudo code.
    - Create arrays and objects I think are needed.
    - Add start button initial functionality.
    - Add showQuestion() initial functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,63 @@ var mainHeaderDiv = document.querySelector("#mainHeader");
 var startButton = document.querySelector("#startButton");
 var information = document.querySelector("#information");
 var possibleAnswers = document.querySelector("#answer");
+var answerList = document.querySelector("#answerList");
 console.log(mainHeaderDiv);
-console.log(startButton);
+console.log(answerList.children);
 
+// Array of Question Headers
+var questionHeader = ["Question 1", "Question 2", "Question 3", "Question 4"];
+// Array of Questions
+var question = ["Which of these is not an option for the display CSS feature?", "What "]
+// Object with multiple choice answers per question in arrays
+var answers = {
+    question1: [`flexbox`, `block`, `inline`, `flipbox`],
+    question2: [`a`, `b`, `c`, `d`],
+    question3: [`a`, `b`, `c`, `d`],
+    question4: [`a`, `b`, `c`, `d`],
+    question5: [`a`, `b`, `c`, `d`]
+}
+
+console.log(answers);
+
+// Event Listener for Start Button
 startButton.addEventListener("click", startQuiz);
 
+// Starts quiz on start button click.
 function startQuiz(event) {
     console.log("hello world");
-    mainHeaderDiv.textContent = "";
-    information.textContent = "";
+    // Tracks how many questions have been asked.
+    var questionCounter = 0;
+    // Remove start button
     startButton.style.display = "none";
+    // Replace initial page text with first question
+    showQuestion(questionCounter);
+    // Start timer (interval)
 }
+
+// Shows a quiz question based on which question was previously answered
+function showQuestion(questionCounter) {
+    switch (questionCounter) {
+        case 0:
+            mainHeaderDiv.textContent = questionHeader[questionCounter];
+            information.textContent = question[questionCounter];
+            questionCounter++;
+            break;
+        case 1:
+            mainHeaderDiv.textContent = "Question 1";
+            information.textContent = "Which of these is not an option for the display CSS feature?";
+            questionCounter++;
+            break;
+        default:
+            break;
+    }
+}
+// Check answer for right or wrong (click eventlistener)
+// Reduce timer by 10 seconds if answer is incorrect
+
+// Proceed to the next each question regardless of answer
+
+// After final question, ask user to input initials
+// Open new page to display score
+// Store score and name in local storage
+// New page button leads back to main page
