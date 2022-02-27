ab7f6d20123db381dab7470f7585e7e4cb54a1e4
Miguel Escobar <mae2136@gmail.com>
2022-02-26 21:34:08 -0600

    script.js: - Fix timer/scoring bugs. - Refactor checkAnswer function to decrease score at final question. - Start local storage functionality. - Fix question array.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,19 +14,22 @@ var storage = {
     Score: [],
 };
 
+localStorage.setItem("scoreHistory", JSON.stringify(storage));
+
 // Array of Question Headers
-var questionHeader = ["Question 1", "Question 2", "Question 3", "Question 4"];
+var questionHeader = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5"];
 // Array of Questions
-var question = ["Which of these is not an option for the display CSS feature?", "What ", "Who", "When", "Why"];
+var question = ["Which of these is not an option for the display CSS feature?", "What API allows you to pull from a Javascript library?", "Which is the operator for equal value and equal type?", "What syntax returns a random number?", "Which method traverses to an element with an id of mainText?"];
 // Object with multiple choice answers per question in arrays
 var answers = {
     question1: [`flexbox`, `block`, `inline`, `flipbox`],
-    question2: [`a`, `b`, `c`, `d`],
-    question3: [`a`, `b`, `c`, `d`],
-    question4: [`a`, `b`, `c`, `d`],
-    question5: [`a`, `b`, `c`, `d`],
-    answerKey: [`flipbox`, `a`, `c`, `a`, `b`],
+    question2: [`JQuery`, `Moment.js`, `day.js`, `Bootstrap`],
+    question3: [`==`, `=`, `===`, `>=`],
+    question4: [`Math.random()`, `random()`, `Math()`, `Math.floor()`],
+    question5: [`querySelector(.mainText)`, `getElementByID(mainText)`, `createElement(mainText)`, `createTextNode(#mainText)`],
+    answerKey: [`flipbox`, `JQuery`, `===`, `Math.random()`, `getElementByID(mainText)`],
 }
+
 // Tracks how many questions have been asked.
 var questionCounter;
 var secondsLeft = 60;
@@ -53,19 +56,18 @@ function startQuiz() {
     answerListDisplay.style.display = "block";
     questionCounter = showQuestion(questionCounter);
     // Start timer (interval) for 60 seconds
-    secondsLeft = startTimer(secondsLeft);
+    startTimer(secondsLeft);
 }
 
-function startTimer(secondsLeft) {
-    timerInterval = setInterval(function() {
+function startTimer() {
+    timerInterval = setInterval(function () {
         secondsLeft--;
         timerEl.textContent = secondsLeft + " seconds left.";
         // If timer = 0, ask user to input initials with score of 0
-        if(secondsLeft === 0) {
+        if (secondsLeft === 0) {
             score(timerInterval, secondsLeft);
         }
     }, 1000);
-    return secondsLeft; 
 }
 
 // Shows a quiz question based on which question was previously answered
@@ -76,7 +78,6 @@ function showQuestion(questionCounter) {
             information.textContent = question[questionCounter];
             for (let i = 0; i < answerList.length; i++) {
                 answerList[i].textContent = answers.question1[i];
-                console.log(answerList[i]);
             }
             break;
         case 1:
@@ -84,7 +85,6 @@ function showQuestion(questionCounter) {
             information.textContent = question[questionCounter];
             for (let i = 0; i < answerList.length; i++) {
                 answerList[i].textContent = answers.question2[i];
-                console.log(answerList[i]);
             }
             break;
         case 2:
@@ -92,7 +92,6 @@ function showQuestion(questionCounter) {
             information.textContent = question[questionCounter];
             for (let i = 0; i < answerList.length; i++) {
                 answerList[i].textContent = answers.question3[i];
-                console.log(answerList[i]);
             }
             break;
         case 3:
@@ -100,7 +99,6 @@ function showQuestion(questionCounter) {
             information.textContent = question[questionCounter];
             for (let i = 0; i < answerList.length; i++) {
                 answerList[i].textContent = answers.question4[i];
-                console.log(answerList[i]);
             }
             break;
         case 4:
@@ -108,7 +106,6 @@ function showQuestion(questionCounter) {
             information.textContent = question[questionCounter];
             for (let i = 0; i < answerList.length; i++) {
                 answerList[i].textContent = answers.question5[i];
-                console.log(answerList[i]);
             }
             break;
         default:
@@ -122,13 +119,13 @@ function showQuestion(questionCounter) {
 function checkAnswer(event) {
     // If correct, append Correct text to bottom of list, else, append Wrong to bottom of list
     document.getElementById("buttonList").appendChild(messageDiv);
+    if (event.target.innerHTML === answers.answerKey[questionCounter - 1]) {
+        messageDiv.appendChild(correct);
+    } else {
+        messageDiv.appendChild(lose);
+        secondsLeft = decreaseTimer(secondsLeft);
+    }
     if (questionCounter < 5) {
-        if (event.target.innerHTML === answers.answerKey[questionCounter-1]) {
-            messageDiv.appendChild(correct);
-        } else {
-            messageDiv.appendChild(lose);
-            secondsLeft = decreaseTimer(secondsLeft);
-        }
         questionCounter = showQuestion(questionCounter);
     } else {
         secondsLeft = score(timerInterval, secondsLeft);
@@ -140,6 +137,7 @@ function checkAnswer(event) {
 // Reduce timer by 10 seconds if answer is incorrect
 function decreaseTimer(secondsLeft) {
     secondsLeft -= 10;
+    timerEl.textContent = secondsLeft + " seconds left.";
     console.log(secondsLeft);
     return secondsLeft;
 }
@@ -166,5 +164,8 @@ function score(timerInterval, secondsLeft) {
 }
 
 // Home Page button leads back to main page
-// Open new page to display score
+// Display score from localstorage
+// storage.Initials = JSON.parse(window.localStorage.getItem(`Initials`));
+// storage.Score = JSON.parse(window.localStorage.getItem(`Score`));
+
 // window.open("https://mae2136.github.io/code-quiz/");
